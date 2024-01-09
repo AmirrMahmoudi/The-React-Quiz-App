30bf8c915a70b34e68337af4f0fa2f927fd611d4
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-01-09 17:05:31 +0330

    initial implementation App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import Main from "./Main";
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 };
