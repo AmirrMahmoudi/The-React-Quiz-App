62b079392f5e3b5b47ecb49fe9691c1b98eb5a85
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-01-10 16:20:42 +0330

    Handling New Answers

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ const initialState = {
   //"Loading","error","ready","active","finished"
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -26,12 +28,22 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOprion
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error("Action unkonwn");
   }
 }
 const App = () => {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -55,7 +67,13 @@ const App = () => {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
