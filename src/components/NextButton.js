b450b100cba827f3cfbbe9f65147c53ed103b0b4
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-01-10 19:31:53 +0330

    Finishing a Quiz

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,16 +1,27 @@
 import React from "react";
 
-const NextButton = ({ dispatch, answer }) => {
+const NextButton = ({ dispatch, answer, index, numQuestions }) => {
   if (answer === null) return null;
 
+  //   if (index < numQuestions - 1)
   return (
     <>
-      <button
-        className="btn btn-ui"
-        onClick={() => dispatch({ type: "nextQuestion" })}
-      >
-        Next
-      </button>
+      {index < numQuestions - 1 && (
+        <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: "nextQuestion" })}
+        >
+          Next
+        </button>
+      )}{" "}
+      {index === numQuestions - 1 && (
+        <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: "finish" })}
+        >
+          Finish
+        </button>
+      )}{" "}
     </>
   );
 };
